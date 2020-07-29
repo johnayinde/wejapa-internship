a55b3963224bae5b034f74673517d4487b38e445
Ayinde John <38318744+johnayinde@users.noreply.github.com>
2020-07-29 03:30:13 +0100

    Update jsonObj.js

--- jsonObj.js
+++ jsonObj.js
@@ -4,7 +4,7 @@ const http = require('http')
 const hostname = '127.0.0.1'
 const port = process.env.PORT || 5000;
 const server = http.createServer((req, res) => {
-  res.statusCode = 200
+  res.statusCode = 201
   res.writeHead(201, { 'Content-Type': 'application/json' });
 Cres.write(JSON.stringify({ success: true, message: ' resource has been created' })); 
 res.end();
